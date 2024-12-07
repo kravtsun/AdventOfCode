ae67547314312d57c4a6598d4350975668301777
Andrey Kravtsun <andrey.kravtsun@dxc.com>
2024-12-07 11:54:12 +0100

    2024: solution7: star2

--- 2024/solution7/main.hs
+++ 2024/solution7/main.hs
@@ -12,27 +12,36 @@ readNumbers line = let
     numbers = map readInt (tail tokens)
     in (expectedResult, numbers)
 
-isGood :: (Int, [Int]) -> Bool
-isGood (expectedResult, numbers) = let
+isGood :: (Int, [Int]) -> Bool -> Bool
+isGood (expectedResult, numbers) isSecondStar = let
     isGoodHelper :: [Int] -> Int -> Int -> Bool
-    isGoodHelper numbers currentSum expectedSum = 
-        if (null numbers) then (currentSum == expectedSum) else let 
+    isGoodHelper numbers currentSum expectedSum | currentSum > expectedSum = False
+                                                | null numbers = currentSum == expectedSum
+                                                | otherwise = let 
             first = head numbers
             others = tail numbers
             isGoodWithMul = isGoodHelper others (currentSum * first) expectedSum
             isGoodWithAdd = isGoodHelper others (currentSum + first) expectedSum
-        in isGoodWithMul || isGoodWithAdd
+            isGoodWithConcat = isGoodHelper others (readInt ((show currentSum) ++ (show first))) expectedSum
+        in isGoodWithMul || isGoodWithAdd || (isSecondStar && isGoodWithConcat)
     in isGoodHelper (tail numbers) (head numbers) expectedResult
 
-
-star1 :: String -> IO ()
-star1 filepath = do
+solve :: String -> Bool -> IO ()
+solve filepath isSecondStar = do
     content <- readFile filepath
     let ls = lines content
     let numbersLines = map readNumbers ls
-    let goodResults = map (\t@(expectedResult, numbers) -> if (isGood t) then expectedResult else 0) numbersLines
+    let goodResults = map (\t@(expectedResult, numbers) -> if (isGood t isSecondStar) then expectedResult else 0) numbersLines
     putStrLn (show (sum goodResults))
 
+star1 :: String -> IO ()
+star1 filepath = solve filepath False
+
+star2 :: String -> IO ()
+star2 filepath = solve filepath True
 
 main :: IO ()
-main = star1 "input.txt"
+-- main = star1 "example_input.txt"
+-- main = star1 "input.txt"
+-- main = star2 "example_input.txt"
+main = star2 "input.txt"
