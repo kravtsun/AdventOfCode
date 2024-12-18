3e0a70b2fedda031939d0eb4f5cecf6137fb4ae2
Andrey Kravtsun <andrey.kravtsun@dxc.com>
2024-12-18 17:59:18 +0100

    with graph

--- 2024/solution6/main.cpp
+++ 2024/solution6/main.cpp
@@ -8,20 +8,7 @@
 #include <set>
 #include <unordered_set>
 
-static const auto LEFT = '<';
-static const auto RIGHT = '>';
-static const auto DOWN = 'v';
-static const auto UP = '^';
-static const std::array dirs = {LEFT, UP, RIGHT, DOWN};
-static const auto ndirs = static_cast<int>(dirs.size());
 using pii = std::pair<int, int>;
-
-static const std::map<char, pii> leads_where = {
-        {LEFT,  {0,  -1}},
-        {RIGHT, {0,  1}},
-        {UP,    {-1, 0}},
-        {DOWN,  {1,  0}},
-};
 
 static bool isGoodCoordinate(int x, int n) {
     return 0 <= x && x < n;
@@ -31,7 +18,30 @@ static bool isGoodPoint(pii p, int n, int m) {
     return isGoodCoordinate(p.first, n) && isGoodCoordinate(p.second, m);
 }
 
-static void print_field(const std::vector<std::string> &lines) {
+static pii addPoint(const pii &lhs, const pii &rhs) {
+    return std::make_pair(lhs.first + rhs.first, lhs.second + rhs.second);
+}
+
+static pii subtractPoint(const pii &lhs, const pii &rhs) {
+    return std::make_pair(lhs.first - rhs.first, lhs.second - rhs.second);
+}
+
+static const auto LEFT = '<';
+static const auto RIGHT = '>';
+static const auto DOWN = 'v';
+static const auto UP = '^';
+static const std::array dirs = {LEFT, UP, RIGHT, DOWN};
+static const auto ndirs = static_cast<int>(dirs.size());
+
+static const std::map<char, pii> dirToDelta = {
+        {LEFT,  {0,  -1}},
+        {RIGHT, {0,  1}},
+        {UP,    {-1, 0}},
+        {DOWN,  {1,  0}},
+};
+
+// TODO remove
+static void printField(const std::vector<std::string> &lines) {
     for (const auto &l: lines) {
         std::cout << l << std::endl;
     }
@@ -70,16 +80,16 @@ static int star1(std::vector<std::string> lines, pii start) {
     const auto startPose = lines[p.first][p.second];
     auto ipose = std::distance(dirs.begin(), std::find(dirs.begin(), dirs.end(), startPose));
 
-    auto where = leads_where.at(dirs[ipose]);
+    auto delta = dirToDelta.at(dirs[ipose]);
 
     while (true) {
         lines[p.first][p.second] = 'X';
-        pii next{p.first + where.first, p.second + where.second};
+        pii next{p.first + delta.first, p.second + delta.second};
         if (isGoodCoordinate(next.first, n) && isGoodCoordinate(next.second, m)) {
             if (lines[next.first][next.second] == '#') {
                 ipose = (ipose + 1) % ndirs;
-                where = leads_where.at(dirs[ipose]);
-                next = std::make_pair(p.first + where.first, p.second + where.second);
+                delta = dirToDelta.at(dirs[ipose]);
+                next = addPoint(p, delta);
             }
 
             p = next;
@@ -102,39 +112,9 @@ static auto star1(const std::string &filepath) {
     return star1(lines, start);
 }
 
-static bool hasLoop(const std::vector<std::string> &lines, pii start) {
-    const int n = static_cast<int>(lines.size());
-    const int m = static_cast<int>(lines[0].size());
-    std::unordered_set<long> used;
-
-    const auto startPose = lines[start.first][start.second];
-    auto ipose = std::distance(dirs.begin(), std::find(dirs.begin(), dirs.end(), startPose));
-    auto where = leads_where.at(dirs[ipose]);
-
-    auto p = start;
-    while (true) {
-        const auto state_number = p.first * m * ndirs + p.second * ndirs + ipose;
-        if (used.count(state_number))
-            return true;
-        used.insert(state_number);
-        pii next{p.first + where.first, p.second + where.second};
-        if (!isGoodCoordinate(next.first, n) || !isGoodCoordinate(next.second, m)) break;
-
-        if (lines[next.first][next.second] == '#') {
-            ipose = (ipose + 1) % ndirs;
-            where = leads_where.at(dirs[ipose]);
-            continue;
-        }
-
-        p = next;
-    }
-
-    return false;
-}
-
 struct State {
     pii p;
-    int dir;
+    int dir{};
 
     friend bool operator==(const State &lhs, const State &rhs) {
         return lhs.p == rhs.p && lhs.dir == rhs.dir;
@@ -157,14 +137,6 @@ struct State {
 const pii INFINITY_POINT{-1, -1}; // This way we denote point of escaping.
 const State INFINITY_STATE{INFINITY_POINT, -1};
 
-static pii add_point(const pii &lhs, const pii &rhs) {
-    return std::make_pair(lhs.first + rhs.first, lhs.second + rhs.second);
-}
-
-static pii subtract_point(const pii &lhs, const pii &rhs) {
-    return std::make_pair(lhs.first - rhs.first, lhs.second - rhs.second);
-}
-
 using States = std::vector<std::vector<std::vector<State>>>;
 using Edges = std::map<State, State>;
 
@@ -172,30 +144,37 @@ static auto createGraph(const std::vector<std::string> &lines) {
     const auto n = static_cast<int>(lines.size());
     const auto m = static_cast<int>(lines[0].size());
     // [ndirs][n][m] -> the closest point by given direction by first index
-    States nextStates(ndirs, std::vector<std::vector<State>>(n, std::vector<State>(m,
-                                                                                   INFINITY_STATE))); // TODO remove in favour of edges?
+    // TODO remove in favour of edges?
+    States nextStates(ndirs, std::vector<std::vector<State>>(n, std::vector<State>(m, INFINITY_STATE)));
+
     Edges edges; // TODO C-array
     for (int d = 0; d < ndirs; ++d) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (nextStates[d][i][j] == INFINITY_STATE) {
-                    auto where = leads_where.at(dirs[d]);
+                if (lines[i][j] == '.' && nextStates[d][i][j] == INFINITY_STATE) {
+                    auto delta = dirToDelta.at(dirs[d]);
                     pii startPoint{i, j};
                     pii p = startPoint;
-                    for (; isGoodPoint(p, n, m); p = add_point(p, where));
+                    for (; isGoodPoint(p, n, m) && lines[p.first][p.second] == '.' &&
+                           nextStates[d][p.first][p.second] == INFINITY_STATE; p = addPoint(p, delta));
 
-                    pii prev = subtract_point(p, where);
+                    // last point when the loop above ended
+                    // - before we hit the field's borders, '#' or already visited cell
+                    auto lastPoint = p;
 
                     State nextState;
                     if (!isGoodPoint(p, n, m)) {
+                        // We hit the field's borders
                         nextState = INFINITY_STATE;
+                    } else if (lines[p.first][p.second] == '#') {
+                        pii finishPoint = subtractPoint(p, delta);
+                        nextState = State{finishPoint, (d + 1) % ndirs};
                     } else {
-                        assert(lines[p.first][p.second] == '#');
-                        nextState = State{prev, (d + 1) % ndirs};
+                        assert(lines[p.first][p.second] == '.');
+                        nextState = nextStates[d][p.first][p.second];
                     }
 
-                    pii prevStartPoint = subtract_point(startPoint, where);
-                    for (p = prev; p != prevStartPoint; p = subtract_point(p, where)) {
+                    for (p = startPoint; p != lastPoint; p = addPoint(p, delta)) {
                         nextStates[d][p.first][p.second] = nextState;
                         edges[State{p, d}] = nextState;
                     }
@@ -226,6 +205,7 @@ static int star2(std::vector<std::string> lines, pii start) {
     const auto startDir = lines[start.first][start.second];
     const auto dir = static_cast<int>(std::distance(dirs.begin(), std::find(dirs.begin(), dirs.end(), startDir)));
     assert(lines[start.first][start.second] == UP);
+    lines[start.first][start.second] = '.';
     const State startState{start, dir};
 
     const int n = static_cast<int>(lines.size());
@@ -234,10 +214,10 @@ static int star2(std::vector<std::string> lines, pii start) {
     int result = 0;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            if (lines[i][j] == '.') {
+            if (lines[i][j] == '.' && pii{i, j} != start) {
                 lines[i][j] = '#';
                 const auto edges = createGraph(lines);
-                if (hasLoop(lines, start)) {
+                if (hasLoop(edges, startState)) {
                     result++;
                 }
                 lines[i][j] = '.';
@@ -247,16 +227,17 @@ static int star2(std::vector<std::string> lines, pii start) {
     return result;
 }
 
+// 6, 1705
 static auto star2(const std::string &filepath) {
     auto [lines, start] = readLines(filepath);
     return star2(lines, start);
 }
 
 int main() {
-    std::cout << star1("example_input.txt") << std::endl;
-    std::cout << star1("input.txt") << std::endl;
+//    std::cout << star1("example_input.txt") << std::endl;
+//    std::cout << star1("input.txt") << std::endl;
 
-    std::cout << star2("example_input.txt") << std::endl;
+//    std::cout << star2("example_input.txt") << std::endl;
     std::cout << star2("input.txt") << std::endl;
     return 0;
 }
