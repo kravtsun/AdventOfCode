f2f7752a282110c576f68fae5c664de2d1291857
Andrey Kravtsun <andrey.kravtsun@dxc.com>
2025-12-14 21:00:50 +0100

    solution12

--- 2025/solution12/main.cpp
+++ 2025/solution12/main.cpp
@@ -307,71 +307,6 @@ static auto mergeRanges(vector<pair<T, T>> ranges) {
     return newRanges;
 }
 
-struct Field {
-    explicit Field(const std::string &filepath)
-            : lines(readLines(filepath)) {
-        checkLines();
-    }
-
-    explicit Field(std::vector<std::string> lines)
-            : lines(std::move(lines)) {
-        checkLines();
-    }
-
-    [[nodiscard]] bool isGoodPoint(const Point &p) const {
-        const auto n = height();
-        const auto m = width();
-        return p.y() >= 0 && p.y() < n && p.x() >= 0 && p.x() < m;
-    }
-
-    char &operator[](const Point &p) {
-        if (!isGoodPoint(p)) {
-            throw std::runtime_error("Bad point: " + to_string(p));
-        }
-        return lines[p.y()][p.x()];
-    }
-
-    const char &operator[](const Point &p) const {
-        if (!isGoodPoint(p)) {
-            throw std::runtime_error("Bad point: " + to_string(p));
-        }
-        return lines[p.y()][p.x()];
-    }
-
-    [[nodiscard]] int height() const {
-        return lines.size();
-    }
-
-    [[nodiscard]] int width() const {
-        return lines[0].size();
-    }
-
-    [[nodiscard]] auto symbolLoc(char symbol) const {
-        for (int i = 0; i < height(); ++i) {
-            for (int j = 0; j < width(); ++j) {
-                if (lines[i][j] == symbol) {
-                    return Point{i, j};
-                }
-            }
-        }
-        throw std::runtime_error("Not found symbol: " + std::string(1, symbol));
-//        return {-1, -1};
-    }
-
-private:
-    std::vector<std::string> lines;
-
-    void checkLines() const {
-        assert(!lines.empty());
-        auto w = lines[0].size();
-        for (const auto &l: lines) {
-            if (l.size() != w) {
-                throw std::runtime_error("Inconsistent line: \n" + l);
-            }
-        }
-    }
-};
-
 template<typename T>
 static bool bitGet(T curSwitch, int i) {
     return curSwitch & ((T) 1 << i);
@@ -382,110 +317,87 @@ static void bitSet(T &curSwitch, int i) {
     curSwitch = curSwitch | ((T) 1 << i);
 }
 
-void star1() {
-    std::ifstream fin{EXAMPLE_INPUT_FILE};
-//    std::ifstream fin{INPUT_FILE};
-//    assert(fin.is_open());
 
-    map<string, vector<string>> g;
+const int SHAPE_SIZE = 3;
 
-    string line;
-    while (std::getline(fin, line)) {
-        istringstream is(line);
-        string from;
-        is >> from;
-        assert(from.back() == ':');
-        from.pop_back();
-        string to;
-        while (is >> to) {
-            g[from].push_back(to);
-        }
-    }
+using Shape = int;
 
-    queue<string> q;
-    q.push("you");
-    map<string, int> d;
-    d["you"] = 1;
-    map<string, bool> used;
-    used["you"] = true;
-
-//    std::function<void(string)> DFS;
-//    DFS = [&](string from) {
-//        used[from] = true;
-//        for (auto to : g[from]) {
-//            d[to] += d[from];
-//            if (!used[to]) {
-//                DFS(to);
-//            }
-//        }
-//    };
-//    DFS("you");
-
-    while (!q.empty()) {
-        auto from = q.front();
-        q.pop();
-        for (auto to: g[from]) {
-            d[to] += d[from];
-            if (!used[to]) {
-                used[to] = true;
-                q.push(to);
-            }
-        }
-    }
-
-    for (auto [v, dist]: d) {
-        cout << v << " " << dist << endl;
-    }
-
-//    cout << d["out"] << endl;
+bool shapeBitGet(Shape shape, int i, int j) {
+    assert(i >= 0 && i < SHAPE_SIZE);
+    assert(j >= 0 && j < SHAPE_SIZE);
+    return bitGet(shape, i * SHAPE_SIZE + j);
 }
 
-string cycle_st, cycle_end;
-
-map<string, char> cl;
-map<string, string> p;
-
-bool dfs(const map<string, vector<string>> &g, string v) {
-    cl[v] = 1;
-    if (g.count(v)) {
-        for (auto to: g.at(v)) {
-            if (cl[to] == 0) {
-                p[to] = v;
-                if (dfs(g, to)) return true;
-            } else if (cl[to] == 1) {
-                cycle_end = v;
-                cycle_st = to;
-                return true;
-            }
-        }
-    }
-    cl[v] = 2;
-    return false;
+void shapeBitSet(Shape &shape, int i, int j) {
+    bitSet(shape, i * SHAPE_SIZE + j);
 }
-
-using Shape = vector<string>;
-struct State {
-    int iShape;
-    int iRot;
-};
 
 Shape rotateShape(const Shape &shape) {
-    Shape resShape = shape;
-    for (int i = 0; i < shape.size(); ++i) {
-        for (int j = 0; j < shape[0].size(); ++j) {
-            resShape[j][shape.size() - i - 1] = shape[i][j];
+    Shape resShape = 0;
+    for (int i = 0; i < SHAPE_SIZE; ++i) {
+        for (int j = 0; j < SHAPE_SIZE; ++j) {
+            if (shapeBitGet(shape, i, j)) {
+                shapeBitSet(resShape, j, SHAPE_SIZE - i - 1);
+            }
         }
     }
     return resShape;
 }
 
+Shape flipShape(const Shape &shape) {
+    Shape resShape = 0;
+    for (int i = 0; i < SHAPE_SIZE; ++i) {
+        for (int j = 0; j < SHAPE_SIZE; ++j) {
+            if (shapeBitGet(shape, i, SHAPE_SIZE - 1 - j)) {
+                shapeBitSet(resShape, i, j);
+            }
+        }
+    }
+    return resShape;
+}
+
+vstring shapeToStrings(const Shape &shape) {
+    vstring strings(SHAPE_SIZE, string(SHAPE_SIZE, '.'));
+    for (int i = 0; i < SHAPE_SIZE; ++i) {
+        for (int j = 0; j < SHAPE_SIZE; ++j) {
+            if (shapeBitGet(shape, i, j)) {
+                strings[i][j] = '#';
+            }
+        }
+    }
+    return strings;
+}
+
+Shape stringsToShape(const vstring &strings) {
+    assert(strings.size() == SHAPE_SIZE && strings.front().size() == SHAPE_SIZE);
+    Shape shape = 0;
+    for (int i = 0; i < SHAPE_SIZE; ++i) {
+        for (int j = 0; j < SHAPE_SIZE; ++j) {
+            if (strings[i][j] != '.') {
+                shapeBitSet(shape, i, j);
+            }
+        }
+    }
+    return shape;
+}
+
+int getShapeArea(const Shape &shape) {
+    int res = 0;
+    for (int i = 0; i < SHAPE_SIZE * SHAPE_SIZE; ++i) {
+        if (bitGet(shape, i)) {
+            res++;
+        }
+    }
+    return res;
+}
+
 
 int main() {
-    auto lines = readLines(EXAMPLE_INPUT_FILE);
-//    auto lines = readLines(INPUT_FILE);
+//    auto lines = readLines(EXAMPLE_INPUT_FILE);
+    auto lines = readLines(INPUT_FILE);
 
-    const int SHAPE_SIZE = 3;
-    vector<Shape> shapes;
+    vector<Shape> originalShapes;
+    vector<int> shapeAreas;
     int iLine;
     for (iLine = 0; iLine < lines.size(); ++iLine) {
         const auto &line = lines[iLine];
@@ -495,75 +407,36 @@ int main() {
         is >> num >> c;
         if (c == 'x') break;
         assert(c == ':');
-        vector<string> shape;
+        vector<string> shapeStrings;
         for (int i = 0; i < SHAPE_SIZE; ++i) {
-            shape.push_back(lines[iLine + i + 1]);
-            assert(shape.back().size() == SHAPE_SIZE);
+            shapeStrings.push_back(lines[iLine + i + 1]);
+            assert(shapeStrings.back().size() == SHAPE_SIZE);
         }
-        shapes.push_back(shape);
+        originalShapes.push_back(stringsToShape(shapeStrings));
+        shapeAreas.push_back(getShapeArea(originalShapes.back()));
         iLine += SHAPE_SIZE + 1;
         assert(lines[iLine].empty());
     }
 
-    const int NSHAPES = shapes.size();
-    vector<vector<vstring>> shapeRotations(NSHAPES, vector<vstring>(4));
-    for (int iShape = 0; iShape < NSHAPES; ++iShape) {
-        auto curShape = shapes[iShape];
+    const int N_ORIGINAL_SHAPES = originalShapes.size();
+    vector<set<Shape>> shapes(N_ORIGINAL_SHAPES);
+//    set<Shape> allShapes;
+    for (int iShape = 0; iShape < N_ORIGINAL_SHAPES; ++iShape) {
+        auto curShape = originalShapes[iShape];
         for (int iRot = 0; iRot < 4; ++iRot) {
-            shapeRotations[iShape][iRot] = curShape;
+            shapes[iShape].insert(curShape);
+//            allShapes.insert(curShape);
+            shapes[iShape].insert(flipShape(curShape));
+//            allShapes.insert(flipShape(curShape));
             curShape = rotateShape(curShape);
         }
+        for_each(all(shapes[iShape]), [](const Shape &shape) {
+            auto strings = shapeToStrings(shape);
+            for (auto l : strings) cout << l << endl;
+            cout << endl;
+        });
+        cout << endl;
     }
-
-
-    // bit strings for 3 last strings
-    using StringBits = int64;
-    using LastStrings = tuple<StringBits, StringBits, StringBits>;
-    static_assert(SHAPE_SIZE == std::tuple_size_v<LastStrings>);
-    const auto stringToBits = [](const int fieldWidth, const string &s) {
-        assert(fieldWidth == s.size());
-        assert(s.size() < 64);
-        StringBits res = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] != '.') {
-                bitSet(res, i);
-            }
-        }
-        return res;
-    };
-    const auto bitsToStrings = [](const int fieldWidth, const StringBits &stringBits) {
-        std::string res(fieldWidth, '.');
-        for (int i = 0; i < fieldWidth; ++i) {
-            if (bitGet(stringBits, i)) {
-                res[i] = '#';
-            }
-        }
-        return res;
-    };
-
-    // shape count into integer representation
-    using ShapeCountsBits = int;
-    const auto MAX_SHAPE_COUNT = 100;
-    const auto shapeCountsToBits = [MAX_SHAPE_COUNT](const int nshapes, const vint &shapeCounts) {
-        assert(nshapes == shapeCounts.size());
-        ShapeCountsBits res = 0;
-        for (const auto &sc: shapeCounts) {
-            res *= MAX_SHAPE_COUNT;
-            assert(sc < MAX_SHAPE_COUNT);
-            res += sc;
-        }
-        return res;
-    };
-    const auto bitsToShapeCounts = [](const int nshapes, ShapeCountsBits shapeCountsBits) {
-        vint shapeCounts;
-        for (int i = 0; i < nshapes; ++i) {
-            shapeCounts.push_back(shapeCountsBits % MAX_SHAPE_COUNT);
-            shapeCountsBits /= MAX_SHAPE_COUNT;
-        }
-        reverse(all(shapeCounts));
-        return shapeCounts;
-    };
-
     int res = 0;
     for (int iField = 0; iLine + iField < lines.size(); ++iField) {
         const auto &line = lines[iLine + iField];
@@ -574,171 +447,27 @@ int main() {
         assert(c == 'x');
         is >> c;
         assert(c == ':');
-        vint finalShapeCounts(NSHAPES);
-        for (int i = 0; i < NSHAPES; ++i) {
+        vint finalShapeCounts(N_ORIGINAL_SHAPES);
+        int requiredArea = 0;
+        for (int i = 0; i < N_ORIGINAL_SHAPES; ++i) {
             is >> finalShapeCounts[i];
+            requiredArea += finalShapeCounts[i] * shapeAreas[i];
+        }
+        if (requiredArea <= fieldWidth * fieldHeight) {
+            res++;
         }
 
-        auto emptyFieldString = string(fieldWidth, '.');
-        auto emptyFieldStringBits = stringToBits(fieldWidth, emptyFieldString);
-
-        using DPKey = tuple<LastStrings, ShapeCountsBits, int>;
-        // LastStrings, iShape, baseFieldLineIndexDelta
-        using DPTransition = tuple<LastStrings, int, int>;
-
-        // fieldWidth -> (State, [stateDelta]).
-        map<int, map<LastStrings, std::vector<DPTransition>>> dpTransitions;
-
-        auto startState = DPKey(make_tuple(emptyFieldStringBits,
-                                           emptyFieldStringBits,
-                                           emptyFieldStringBits),
-                                shapeCountsToBits(NSHAPES, finalShapeCounts),
-                                0);
-
-        bool curRes = false;
-        set<DPKey> used;
-        queue<DPKey> q;
-        q.push(startState);
-        used.insert(startState);
-        while (!q.empty()) {
-            auto curDPKey = q.front();
-            const auto [lastStringsBitsTuple, shapeCountsBits, baseFieldLineIndex] = curDPKey;
-            assert(baseFieldLineIndex + SHAPE_SIZE <= fieldHeight);
-            q.pop();
-
-            if (shapeCountsBits == 0) {
-                curRes = true;
-                break;
-            }
-
-
-            auto [lastStringsBits0, lastStringsBits1, lastStringsBits2] = lastStringsBitsTuple;
-//            vstring lastStrings{
-//                    bitsToStrings(fieldWidth, lastStringsBits0),
-//                    bitsToStrings(fieldWidth, lastStringsBits1),
-//                    bitsToStrings(fieldWidth, lastStringsBits2),
-//            };
+//        using Field = vector<vector<Shape>>;
+//        Field field(fieldHeight - SHAPE_SIZE + 1, vector<Shape>(fieldWidth - SHAPE_SIZE + 1, 0));
+//        auto setShape = [](const Field &field, int i, int j, Shape shape) {
+//            assert((field[i][j] ^ shape) == 0);
+//            for (int ii = 0; ii < ; ++ii) {
 //
-//            cout << baseFieldLineIndex << endl;
-//            for (const auto& s : lastStrings) {
-//                cout << s << endl;
 //            }
-//            cout << endl;
-            auto shapeCounts = bitsToShapeCounts(NSHAPES, shapeCountsBits);
-            if (dpTransitions.count(fieldWidth) && dpTransitions[fieldWidth].count(lastStringsBitsTuple)) {
-                const auto &curDPTransitions = dpTransitions.at(fieldWidth).at(lastStringsBitsTuple);
-                for (const auto &dpTransition : curDPTransitions) {
-                    auto [nextLastStringsBitsTuple, nextIShape, nextBaseFieldLineIndexDelta] = dpTransition;
-                    if (shapeCounts[nextIShape] == 0) continue;
-                    if (baseFieldLineIndex + nextBaseFieldLineIndexDelta + SHAPE_SIZE >= fieldHeight) continue;
-                    shapeCounts[nextIShape]--;
-                    auto nextShapeCountsBits = shapeCountsToBits(NSHAPES, shapeCounts);
-                    shapeCounts[nextIShape]++;
-                    auto nextBaseFieldLineIndex = baseFieldLineIndex + nextBaseFieldLineIndexDelta;
-                    DPKey nextState{nextLastStringsBitsTuple, nextShapeCountsBits, nextBaseFieldLineIndex};
-                    if (used.count(nextState) == 0) {
-                        used.insert(nextState);
-                        q.push(nextState);
-                    }
-                }
-                continue;
-            }
-
-//            vstring lastStrings{
-//                    bitsToStrings(fieldWidth, lastStringsBits0),
-//                    bitsToStrings(fieldWidth, lastStringsBits1),
-//                    bitsToStrings(fieldWidth, lastStringsBits2),
-//            };
-//
-//            cout << baseFieldLineIndex << endl;
-//            for (const auto& s : lastStrings) {
-//                cout << s << endl;
-//            }
-//            cout << endl;
-
-            vector<StringBits> lastStringsBits{lastStringsBits0, lastStringsBits1, lastStringsBits2};
-            for (int i = 0; i < SHAPE_SIZE; ++i) {
-                lastStringsBits.push_back(emptyFieldStringBits);
-//                lastStrings.push_back(emptyFieldString);
-            }
-
-            for (int iShape = 0; iShape < NSHAPES; ++iShape) {
-//                if (shapeCounts[iShape] == 0) continue;
-//                shapeCounts[iShape]--;
-//                auto shapeCountsBitsNext = shapeCountsToBits(NSHAPES, shapeCounts);
-                for (int i = 0; i <= SHAPE_SIZE && baseFieldLineIndex + i + SHAPE_SIZE <= fieldHeight; ++i) {
-//                    vstring lastStringsI{lastStrings[i],
-//                                         lastStrings[i + 1],
-//                                         lastStrings[i + 2]};
-                    vector<StringBits> lastStringsBitsI{
-                            lastStringsBits[i],
-                            lastStringsBits[i + 1],
-                            lastStringsBits[i + 2],
-                    };
-                    for (int iRot = 0; iRot < 4; ++iRot) {
-                        const auto &curShape = shapeRotations[iShape][iRot];
-                        for (int j = 0; j + SHAPE_SIZE <= fieldWidth; ++j) {
-//                            auto newLastStrings = lastStringsI;
-                            auto newLastStringsBits = lastStringsBitsI;
-                            bool shapeCanFit = true;
-                            for (int ii = 0; ii < SHAPE_SIZE && shapeCanFit; ++ii) {
-                                for (int jj = 0; jj < SHAPE_SIZE && shapeCanFit; ++jj) {
-                                    if (curShape[ii][jj] == '.') continue;
-//                                    if (newLastStrings[ii][j + jj] != '.') {
-//                                        shapeCanFit = false;
-//                                    } else {
-//                                        newLastStrings[ii][j + jj] = char('A' + iShape);
-//                                    }
-                                    if (bitGet(newLastStringsBits[ii], j + jj)) {
-                                        shapeCanFit = false;
-                                    } else {
-                                        bitSet(newLastStringsBits[ii], j + jj);
-                                    }
-                                }
-                            }
-                            if (shapeCanFit) {
-//                                LastStrings lastStringsBitsTupleNext{
-//                                        stringToBits(fieldWidth, newLastStrings[0]),
-//                                        stringToBits(fieldWidth, newLastStrings[1]),
-//                                        stringToBits(fieldWidth, newLastStrings[2])};
-                                LastStrings lastStringsBitsTupleNext = {
-                                        newLastStringsBits[0],
-                                        newLastStringsBits[1],
-                                        newLastStringsBits[2],
-//                                        stringToBits(fieldWidth, newLastStrings[0]),
-//                                        stringToBits(fieldWidth, newLastStrings[1]),
-//                                        stringToBits(fieldWidth, newLastStrings[2])
-                                };
-
-
-//                                auto [nextLastStringsBitsTuple, nextIShape, nextBaseFieldLineIndexDelta] = dpTransition;
-                                dpTransitions[fieldWidth][lastStringsBitsTuple].emplace_back(lastStringsBitsTupleNext, iShape, i);
-
-                                if (shapeCounts[iShape] != 0) {
-                                    shapeCounts[iShape]--;
-                                    auto shapeCountsBitsNext = shapeCountsToBits(NSHAPES, shapeCounts);
-                                    auto nextState = DPKey{lastStringsBitsTupleNext, shapeCountsBitsNext,
-                                                           baseFieldLineIndex + i};
-                                    if (!used.count(nextState)) {
-                                        q.push(nextState);
-                                        used.insert(nextState);
-                                    }
-                                    shapeCounts[iShape]++;
-                                }
-                            }
-                        }
-                    }
-                }
-//                shapeCounts[iShape]++;
-            }
-        }
-
-        cout << iField << " " << (curRes ? " OK " : "FAIL") << endl;
-        res += curRes;
+//        };
     }
 
     cout << res << endl;
-
 
     return 0;
 }
